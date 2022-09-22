b91b0cbab13bdecacc1295e1edebeb4b9afb9364
timur <yusupov-rr22@mail.ru>
2022-09-22 22:52:41 +0700

    res = num * num

---
+++ less001_c#/part2_квадрат числа/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num * num;
+ Console.WriteLine($"Квадрат числа {num}: {result}");
+
+ if (num == result)
+
+ {
+    Console.Write("1 ");
+ }
+ else
+ {
+    Console.Write("2 ");
+ }
