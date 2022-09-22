7d41273b6987b1d84eed4496b9700a9d7c7bdd7b
timur <yusupov-rr22@mail.ru>
2022-09-22 23:34:37 +0700

    num1 =? num2 * num2

---
+++ less001_c#/part3_а равно ли б умножитьб/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите число1:  ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число2:  ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if ((num1 == num2 * num2) || (num2 == num1 * num1))
+
+{
+   Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+
