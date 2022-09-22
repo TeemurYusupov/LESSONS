681d0ef829a0058afd23f3610c69fa9cddc3e403
timur <yusupov-rr22@mail.ru>
2022-09-23 00:14:49 +0700

    дни недели по номеру

---
+++ less001_c#/part4_дни недели по номеру/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+Console.Write("Введите число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ( num == 1)
+{
+    Console.WriteLine("понедельник");
+}
+if ( num == 2)
+{
+    Console.WriteLine("вторник");
+}
+if ( num == 3)
+{
+    Console.WriteLine("среда");
+}
+if ( num == 4)
+{
+    Console.WriteLine("четверг");
+}
+if ( num == 5)
+{
+    Console.WriteLine("пятница");
+}
+if ( num == 6)
+{
+    Console.WriteLine("суббота");
+}
+if ( num == 7)
+{
+    Console.WriteLine("воскресенье");
+}
+
